bb580ac309deb34159f8b9efd8f56fcad346c828
Julià <jcolomr18@gmail.com>
2017-02-06 20:23:31 +0100

    constructor amb string casi acabat falta comprovar que si ha dos velors iguals mels sumi

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,3 +1,5 @@
+import org.hamcrest.core.SubstringMatcher;
+
 import java.text.DecimalFormat;
 import java.util.Arrays;
 
@@ -28,12 +30,14 @@ public class Polynomial {
         int cont=0;
         //Eliminal els lignes i si es negatiu l'afarram al valor
         for (int i = 0; i <cffs.length ; i++) {
+            //si no es positiu affaram el signa al numero
             if(cffs[i].contains("+")){ cffs[i]= null;}else if(cffs[i].contains("-")&&cffs[i].length()<2){
                 cffs[i+1]="-"+cffs[i+1];
                 cffs[i]=null;
             }
             if(cffs[i]!= null){cont++;}
         }
+        //Eliminam els nulls
         cffsc=new String[cont];
         for (int i = 0, y=0; i <cffs.length ; i++) {
             if(cffs[i]!= null){
@@ -41,10 +45,64 @@ public class Polynomial {
                 y++;
             }
         }
+        //pot ser que hi hagui codi repetit
+       int[] exp = new int[cffsc.length];
+        float [] vari=new float[cffsc.length];
+        for (int i = 0; i <cffsc.length ; i++) {
+           int n= cffsc[i].indexOf("^");
+            int p= cffsc[i].indexOf("x");
+            n++;
+            if (cffsc[i].contains("x^")){
+                exp[i]=Integer.parseInt( cffsc[i].substring(n));
+               //comprobam si la variable es 0 que me posi un 1 o -1
+                if(cffsc[i].substring(0,p).equals("-")) {
+                    vari[i] = -1;
+                    continue;
+                } else if(p==0) {
+                    vari[i] = 1;
+                    continue;
+                }
+                vari[i]=Integer.parseInt( cffsc[i].substring(0,p));
+                continue;
+            }else if(cffsc[i].contains("x")){
+                exp[i]=1;
+                //comprobam si la variable es 0 que me posi un 1 o -1
+                if(cffsc[i].substring(0,p).equals("-")) {
+                    vari[i] = -1;
+                    continue;
+                } else if(p==0) {
+                vari[i] = 1;
+                continue;
+                }
+                vari[i]=Integer.parseInt( cffsc[i].substring(0,p));
 
+
+            }else {vari[i]=Integer.parseInt( cffsc[i]);}
+
+        }
+        //volem sebre quin es la potencia mes alta
+        //per poder crear un arrai de la dimensio
+        int x=0;
+        for (int i = 0; i <exp.length ; i++) {
+            if(exp[i]>x){x=exp[i];}
+        }
+        //colocam els verlors per els seus exponents
+       float[] cfso=new float[x+1];
+        for (int i = 0; i <vari.length; i++) {
+            cfso[exp[i]]=vari[i];
+
+        }
+        cfs=new float[x+1];
+        //Giram l'array per poderla pasar al toString
+        for (int i = x, d=0; i >=0 ; i--, d++) {
+            cfs[d]=cfso[i];
+        }
+
+
+        System.out.println(Arrays.toString(exp));
         System.out.println(Arrays.toString(cffsc));
-        cfs = new float[1];
-        cfs[0]= 0;
+        System.out.println(Arrays.toString(cfs));
+
     }
 
     // Suma el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
@@ -94,6 +152,8 @@ public class Polynomial {
                 if(ccc[i]==0){exp--; continue;}
                 if(ccc[i]==1&&exp==1){sb.append("x"); continue;}
                 if(exp==1){sb.append(ccc[i]+"x"); continue;}
+                if(ccc[i]==1&&i==0){sb.append("x^" + exp); exp--; continue;}
+                if(ccc[i]==-1&&i==0){sb.append("-x^" + exp); exp--; continue;}
                 sb.append(ccc[i] + "x^" + exp);
                 exp--;
                 continue;
